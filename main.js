b873ad01edd58e96adc9b034ad92c980aad6e873
ManuelCaroDev <manuelcaro.dev@gmail.com>
2025-08-13 23:09:37 +0200

    first_commit

---
+++ main.js
@@ -0,0 +1,367 @@
+const bmwMModelsEspana = [
+  {
+    modelo: "M8 Gran Coupé",
+    potencia: "460 kW (625 CV)",
+    precio: "215.400€",
+    version: "M",
+    carroceria: "gran coupé",
+    imgUrl: "./assets/images/m8grandcoupe.webp",
+  },
+  {
+    modelo: "M8 Cabrio Competition",
+    potencia: "460 kW (625 CV)",
+    precio: "227.400€",
+    version: "M Competition",
+    carroceria: "cabrio",
+    imgUrl: "./assets/images/m8cabrio2.webp",
+  },
+  {
+    modelo: "M8 Coupé Competition",
+    potencia: "460 kW (625 CV)",
+    precio: "218.650€",
+    version: "M Competition",
+    carroceria: "coupé",
+    imgUrl: "./assets/images/m8coupe.webp",
+  },
+  {
+    modelo: "M5 Berlina",
+    potencia: "535 kW (727 CV)",
+    precio: "161.000€",
+    version: "M",
+    carroceria: "berlina",
+    imgUrl: "./assets/images/m5berlina.webp",
+  },
+  {
+    modelo: "M5 Touring",
+    potencia: "535 kW (727 CV)",
+    precio: "163.600€",
+    version: "M",
+    carroceria: "familiar/touring",
+    imgUrl: "./assets/images/m5touring.webp",
+  },
+
+  {
+    modelo: "M4 Cabrio",
+    potencia: "390 kW (530 CV)",
+    precio: "135.850€",
+    version: "M",
+    carroceria: "cabrio",
+    imgUrl: "./assets/images/m4cabrio.webp",
+  },
+  {
+    modelo: "M4 Coupé",
+    potencia: "390 kW (530 CV)",
+    precio: "126.500€",
+    version: "M",
+    carroceria: "coupé",
+    imgUrl: "./assets/images/m4.webp",
+  },
+  {
+    modelo: "M3 Competition Berlina",
+    potencia: "390 kW (530 CV)",
+    precio: "124.450€",
+    version: "M Competition",
+    carroceria: "berlina",
+    imgUrl: "./assets/images/m3berlina.webp",
+  },
+  {
+    modelo: "BMW M3 Competition Touring",
+    potencia: "390 kW (530 CV)",
+    precio: "128.945,09€",
+    version: "M Competition",
+    carroceria: "familiar/touring",
+    imgUrl: "./assets/images/m3touring.webp",
+  },
+  {
+    modelo: "Nuevo BMW M2 Coupé",
+    potencia: "353 kW (480 CV)",
+    precio: "94.500€",
+    version: "M",
+    carroceria: "coupé",
+    imgUrl: "./assets/images/m2.webp",
+  },
+  {
+    modelo: "Z4 M40i Roadster",
+    potencia: "250 kW (340 CV)",
+    precio: "79.200€",
+    version: "M Performance",
+    carroceria: "roadster",
+    imgUrl: "./assets/images/z4.webp",
+  },
+  {
+    modelo: "M440i xDrive Gran Coupé",
+    potencia: "275 kW (374 CV)",
+    precio: "82.300€",
+    version: "M Performance",
+    carroceria: "gran coupé",
+    imgUrl: "./assets/images/m440i.webp",
+  },
+  {
+    modelo: "M135i xDrive",
+    potencia: "125 kW (170 CV)",
+    precio: "61.000€",
+    version: "M Performance",
+    carroceria: "hatchback",
+    imgUrl: "./assets/images/m135i.webp",
+  },
+  {
+    modelo: "iX M70",
+    potencia: "485 kW (659 CV)",
+    precio: "132.550€",
+    version: "M Performance",
+    carroceria: "SUV",
+    imgUrl: "./assets/images/iXm70.webp",
+  },
+
+  {
+    modelo: "XM",
+    potencia: "550 kW (748 CV)",
+    precio: "134.100€",
+    version: "M",
+    carroceria: "SUV",
+    imgUrl: "./assets/images/xm.webp",
+  },
+  {
+    modelo: "X7 M60i",
+    potencia: "390 kW (530 CV)",
+    precio: "147.950€",
+    version: "M Performance",
+    carroceria: "SUV",
+    imgUrl: "./assets/images/x7m60i.webp",
+  },
+  {
+    modelo: "X6 M",
+    potencia: "460 kW (625 CV)",
+    precio: "180.750€",
+    version: "M",
+    carroceria: "SUV coupé",
+    imgUrl: "./assets/images/x6m.webp",
+  },
+  {
+    modelo: "X5 M",
+    potencia: "460 kW (625 CV)",
+    precio: "177.250€",
+    version: "M",
+    carroceria: "SUV",
+    imgUrl: "./assets/images/x5m.webp",
+  },
+  {
+    modelo: "X4 M",
+    potencia: "353 kW (480 CV)",
+    precio: "121.750€",
+    version: "M",
+    carroceria: "SUV coupé",
+    imgUrl: "./assets/images/x4m.webp",
+  },
+  {
+    modelo: "X3 M50 xDrive",
+    potencia: "293 kW (398 CV)",
+    precio: "89.000€",
+    version: "M Performance",
+    carroceria: "SUV",
+    imgUrl: "./assets/images/x3m50.webp",
+  },
+  {
+    modelo: "X2 M35i xDrive",
+    potencia: "221 kW (300 CV)",
+    precio: "72.250€",
+    version: "M Performance",
+    carroceria: "SUV coupé",
+    imgUrl: "./assets/images/x2m35i.webp",
+  },
+  {
+    modelo: "X1 M35i xDrive",
+    potencia: "221 kW (300 CV)",
+    precio: "72.700€",
+    version: "M Performance",
+    carroceria: "SUV",
+    imgUrl: "./assets/images/x1m35i.webp",
+  },
+  {
+    modelo: "i5 M60 xDrive",
+    potencia: "442 kW (601 CV)",
+    precio: "114.450€",
+    version: "M Performance",
+    carroceria: "berlina",
+    imgUrl: "./assets/images/i5m60.webp",
+  },
+  {
+    modelo: "i5 M60 xDrive Touring",
+    potencia: "442 kW (601 CV)",
+    precio: "117.050€",
+    version: "M Performance",
+    carroceria: "familiar/touring",
+    imgUrl: "./assets/images/i5m60touring.webp",
+  },
+  {
+    modelo: "i4 M60",
+    potencia: "442 kW (601 CV)",
+    precio: "80.500€",
+    version: "M Performance",
+    carroceria: "gran coupé",
+    imgUrl: "./assets/images/i4m60.webp",
+  },
+  {
+    modelo: "i7 M70 xDrive",
+    potencia: "485 kW (659 CV)",
+    precio: "187.150€",
+    version: "M Performance",
+    carroceria: "berlina",
+    imgUrl: "./assets/images/i7m70i.webp",
+  },
+  {
+    modelo: "M760e xDrive",
+    potencia: "420 kW (571 CV)",
+    precio: "145.200€",
+    version: "M Performance",
+    carroceria: "berlina",
+    imgUrl: "./assets/images/m760e.webp",
+  },
+];
+
+/* Creo dinamicamente el header con un h1 y una imagen */
+const header = document.createElement("header");
+header.innerHTML = `
+ <h1><img src="./assets/images/header.webp" alt="bmw motorsport"></h1>
+`;
+document.body.appendChild(header);
+
+/* Creo dinamicamente el main con las dos section */
+const main = document.createElement("main");
+main.innerHTML = `
+<section id = "filter"></section>
+<section id = "gallery"></section>
+`;
+document.body.appendChild(main);
+
+/* Creo una funcion para pintar una tarjeta por cada objeto del array */
+const allModels = (bmwMModelsEspana) => {
+  const sectionModels = document.getElementById("gallery");
+  sectionModels.innerHTML = "";
+
+  for (const bmw of bmwMModelsEspana) {
+    const article = document.createElement("article");
+    article.classList.add("card");
+
+    const img = document.createElement("img");
+    img.src = bmw.imgUrl;
+    img.alt = bmw.modelo;
+    img.className = "images";
+
+    const div = document.createElement("div");
+    div.className = "articleDiv";
+    div.innerHTML = `
+    <h3>Modelo: ${bmw.modelo}</h3>
+    <p>Potencia: ${bmw.potencia}</p>
+    <span>Desde: ${bmw.precio}</span>
+    `;
+
+    article.appendChild(img);
+    article.appendChild(div);
+    sectionModels.appendChild(article);
+  }
+};
+
+/* Creo los botones para filtral los coches segun el tipo de carroceria, por version y un ultimo boton para pintar todos los coches */
+const buttons = document.getElementById("filter");
+buttons.innerHTML = `
+  <button id="berlina">Berlinas</button>
+  <button id="coupe">Coupes</button>
+  <button id="granCoupe">Gran Coupes</button>
+  <button id="cabrio">Cabrios</button>
+  <button id="suv">Suvs</button>
+  <button id="suvCoupe">Suvs Coupes</button>
+  <button id="touring">Tourings</button>
+  <button id="hatchback">Hatchbacks</button>
+  <button id="roadster">roadsters</button>
+  <button id="m">Modelos M</button>
+  <button id="mPerformance">Modelos Mperf</button>
+  <button id="allModels">Todos</button> 
+`;
+
+/* Creo las funciones para filtrar por carroceria y por version */
+const chassisFilter = (carroceria) => {
+  const carsChassis = bmwMModelsEspana.filter(
+    (bmw) => bmw.carroceria === carroceria
+  );
+  allModels(carsChassis);
+};
+
+const versionFilter = (version) => {
+  const carsVersion = bmwMModelsEspana.filter((bmw) => bmw.version === version);
+  allModels(carsVersion);
+};
+
+/*Aqui selecciono cada boton para despues añadirle a cada uno evento click que llame a la funcion para filtrar los coches  */
+const berlinaBtn = document.getElementById("berlina");
+const coupeBtn = document.getElementById("coupe");
+const granCoupeBtn = document.getElementById("granCoupe");
+const cabrioBtn = document.getElementById("cabrio");
+const suvBtn = document.getElementById("suv");
+const suvCoupeBtn = document.getElementById("suvCoupe");
+const touringBtn = document.getElementById("touring");
+const hatchbackBtn = document.getElementById("hatchback");
+const roadsterBtn = document.getElementById("roadster");
+const mBtn = document.getElementById("m");
+const mPerfBtn = document.getElementById("mPerformance");
+const allModelsBtn = document.getElementById("allModels");
+
+/* Filtros por tipo de carroceria */
+berlinaBtn.addEventListener("click", () => {
+  chassisFilter("berlina");
+});
+
+coupeBtn.addEventListener("click", () => {
+  chassisFilter("coupé");
+});
+
+granCoupeBtn.addEventListener("click", () => {
+  chassisFilter("gran coupé");
+});
+
+cabrioBtn.addEventListener("click", () => {
+  chassisFilter("cabrio");
+});
+
+touringBtn.addEventListener("click", () => {
+  chassisFilter("familiar/touring");
+});
+
+hatchbackBtn.addEventListener("click", () => {
+  chassisFilter("hatchback");
+});
+
+roadsterBtn.addEventListener("click", () => {
+  chassisFilter("roadster");
+});
+
+suvBtn.addEventListener("click", () => {
+  chassisFilter("SUV");
+});
+
+suvCoupeBtn.addEventListener("click", () => {
+  chassisFilter("SUV coupé");
+});
+
+/* Filtro para volver a pintar todos los coches  */
+allModelsBtn.addEventListener("click", () => {
+  allModels(bmwMModelsEspana);
+});
+
+/* Filtro por version */
+mBtn.addEventListener("click", () => {
+  versionFilter("M");
+});
+
+mPerfBtn.addEventListener("click", () => {
+  versionFilter("M Performance");
+});
+
+/* Con esta llamada a la funcion hago que en la primera carga de la pagina se pinten todos los coches */
+allModels(bmwMModelsEspana);
+
+/* Creo el footer */
+const footer = document.createElement("footer");
+footer.innerText =
+  "© 2025. Todos los derechos reservados. Desarrollado por MCDev";
+document.body.appendChild(footer);
